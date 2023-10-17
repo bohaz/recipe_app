1ccdc880f4e76b2e4ac6e879e73e52af88b7a128
dani <danifromecuador@gmail.com>
2023-10-17 13:01:47 -0500

    Add route, controller and view for creating new foods

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -7,4 +7,26 @@ class FoodsController < ApplicationController
   def show
     @food = Food.find(params[:id])
   end
+
+  def new
+    @user = current_user
+    @food = Food.new
+  end
+
+  def create
+    @user = current_user
+    @food = Food.new(food_params)
+    @food.user_id = @user.id
+    if @food.save
+      redirect_to foods_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
-  
-  get 'foods', to: 'foods#index', as: 'foods'
+  resources :foods
   root "users#index"
 end
