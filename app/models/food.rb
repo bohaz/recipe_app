3db068c8b67141ac4a6e88ace67bdf14c010af3f
dani <danifromecuador@gmail.com>
2023-10-17 10:24:38 -0500

    Create validations and associations

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,9 @@
 class Food < ApplicationRecord
   belongs_to :user
+  foreign_key :user_id
+
+  validates :name, presence: true
+  validates :measurement_unit, presence: true
+  validates :price, presence: true
+  validates :quantity, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :foods
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
 end
