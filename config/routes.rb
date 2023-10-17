a292864bb13f21e4dfaf835f61842e0b4527179e
dani <danifromecuador@gmail.com>
2023-10-17 12:36:48 -0500

    Add route, controller, and empty view for foods

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
-
-  # Defines the root path route ("/")
+  
+  get 'foods', to: 'foods#index', as: 'foods'
   root "users#index"
 end
