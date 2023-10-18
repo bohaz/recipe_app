2447e9f5cd8959e3ae858134b8eaca61fe083b13
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-10-17 21:14:49 -0300

    Add relation for recipes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :foods
+  has_many :recipes
 
   # validates :name, presence: true
   validates :email, presence: true
