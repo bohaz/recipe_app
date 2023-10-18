03555430bb3c58387d41b35c013073ae7ff93cd9
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-10-18 15:45:26 -0300

    Add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :foods
-  resources :recipes, except: [:update]
+  
+  resources :recipes, except: [:update] do
+    member do
+      patch :toggle_public
+    end
+    resources :recipe_foods, only: [:new, :create]
+  end
+  
+  resources :recipe_foods, except: [:new, :create, :update]
+
   delete 'foods/:id', to: 'foods#destroy', as: 'delete_food'
-      root "users#index"
+  root "users#index"
 end
