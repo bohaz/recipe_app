52bbca7b2fc62fe6dd4d3c98278d8835afedeeaa
dani <danifromecuador@gmail.com>
2023-10-18 17:46:27 -0500

    Edit shopping list, it's showing all the items but wrong

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -8,6 +8,9 @@ class ShoppingListsController < ApplicationController
 
   def calculate_shopping_list
     shopping_list = {}
+    total_value = 0
+    total_items = 0
+  
     @user.recipes.each do |recipe|
       recipe.recipe_foods.each do |recipe_food|
         food = recipe_food.food
@@ -15,10 +18,13 @@ class ShoppingListsController < ApplicationController
         quantity_owned = @user.foods.find_by(id: food.id)&.quantity.to_i
         difference = quantity_needed - quantity_owned
         if difference > 0
-          shopping_list[food.name] = difference
+          shopping_list[food.name] = { quantity: difference, price: food.price }
+          total_value += difference * food.price
+          total_items += 1
         end
       end
     end
-    shopping_list
+  
+    { shopping_list: shopping_list, total_value: total_value, total_items: total_items }
   end
 end
