1c5a9bf65dfa93d3919889e6b1b384022be259a1
dani <danifromecuador@gmail.com>
2023-10-18 17:14:13 -0500

    Show total price for all public recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,7 @@ class Recipe < ApplicationRecord
   has_many :recipe_foods, dependent: :destroy
   has_many :foods, through: :recipe_foods
   validates :name, presence: true
+  def total_price
+    recipe_foods.sum { |rf| rf.quantity * rf.food.price }
+  end
 end
