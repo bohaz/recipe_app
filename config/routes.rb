430ed4c5e5a5ff4951d8858bfcc1bbc4bf37bf83
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-10-17 21:18:16 -0300

    Add recipe resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :foods
+  resources :recipes, except: [:update]
   delete 'foods/:id', to: 'foods#destroy', as: 'delete_food'
-  root "users#index"
+      root "users#index"
 end
