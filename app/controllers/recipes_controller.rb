1111d271df6d583fc047c930bd707c058a0a21cc
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-10-17 21:13:35 -0300

    Add new recipes controller

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,41 @@
+class RecipesController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @recipes = current_user.recipes
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    if @recipe.user == current_user
+      @recipe.destroy
+      redirect_to recipes_path, notice: 'Recipe was successfully deleted.'
+    else
+      redirect_to recipes_path, alert: 'You are not authorized to delete this recipe.'
+    end
+  end
+  
+
+  def new
+    @recipe = current_user.recipes.new
+  end
+  
+  def create
+    @recipe = current_user.recipes.new(recipe_params)
+    if @recipe.save
+      redirect_to recipes_path, notice: 'Recipe successfully created!'
+    else
+      render :new
+    end
+  end
+  
+  private
+  
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
+  
+end
