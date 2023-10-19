d059265bade15161356eaba30748c635612135fe
dani <danifromecuador@gmail.com>
2023-10-19 11:17:25 -0500

    Fix the total price and the count of items

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -10,21 +10,20 @@ class ShoppingListsController < ApplicationController
     shopping_list = {}
     total_value = 0
     total_items = 0
-
-    @user.recipes.each do |recipe|
-      recipe.recipe_foods.each do |recipe_food|
-        food = recipe_food.food
-        quantity_needed = recipe_food.quantity
-        quantity_owned = @user.foods.find_by(id: food.id)&.quantity.to_i
-        difference = quantity_needed - quantity_owned
-        next unless difference.positive?
-
-        shopping_list[food.name] = { quantity: difference, price: food.price }
-        total_value += difference * food.price
+  
+    @user.foods.each do |food|
+      if food.quantity.negative?
+        quantity_needed = -food.quantity
+        total_value += quantity_needed * food.price
         total_items += 1
+        shopping_list[food.name] = { quantity: quantity_needed, price: food.price }
       end
     end
-
-    { shopping_list:, total_value:, total_items: }
+  
+    {
+      shopping_list: shopping_list,
+      total_value: total_value,
+      total_items: total_items
+    }
   end
 end
